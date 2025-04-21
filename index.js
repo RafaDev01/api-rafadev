33d9d4d13864aea47f8db4bf9da3656b340d2f0a
Rafael do Nascimento Santos <rflwwe123@hotmail.com>
2025-04-21 00:56:57 -0300

    commit

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.use((req, res, next) => {
     next();
 });
 
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Servidor rodando em http://0.0.0.0:${port}`);
 });
